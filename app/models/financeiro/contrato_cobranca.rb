1473b0bb92572c95df192438c08e768047ee7f03
Lesio Pinheiro <lesio.lap@gmail.com>
2014-10-24 15:17:06 -0200

    to_s para contrato cedente

--- app/models/financeiro/contrato_cobranca.rb
+++ app/models/financeiro/contrato_cobranca.rb
@@ -3,5 +3,14 @@ module Financeiro
     belongs_to :conta
     
     scope :ativos, where(ativo: true)
+    
+    # metodo usado na tela de contribuicoes
+    def formatted_name
+      "#{titular} - #{nome_banco} - Ag: #{agencia} - Cedente: #{numero_contrato}"
+    end
+  
+    def to_s
+       "#{conta.titular} - #{conta.banco.nome} - Ag: #{conta.agencia} - Cedente: #{numero_contrato}"
+    end
   end
 end
