78800af69045faa187332395d1c4598adc727133
Lesio Pinheiro <lesio.lap@gmail.com>
2014-10-24 11:32:51 -0200

    fix load spec arquivo

--- app/models/financeiro/tipo_arquivo.rb
+++ app/models/financeiro/tipo_arquivo.rb
@@ -1,5 +1,11 @@
 module Financeiro
   class TipoArquivo < ActiveRecord::Base
     belongs_to :meio_pagamento
+    
+    #Carrega a especificação de como o arquivo deve ser gerado ou importado
+    #para o sistema
+    def load_spec
+      self.extend eval(spec) if self.spec
+    end
   end
 end
